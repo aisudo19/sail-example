eac21e179a5143426456135f9775b4c661928214
aisudo19 <indigonaka@gmail.com>
2025-01-29 14:09:33 +0900

    Seederファイルの作成

--- database/seeders/StepsTableSeeder.php
+++ database/seeders/StepsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class StepsTableSeeder extends Seeder
 {
@@ -12,6 +14,20 @@ class StepsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $recipes = DB::table('recipes')->pluck('id')->toArray();
+
+        foreach ($recipes as $recipeId) {
+            $numberOfSteps = rand(3, 6); // Randomly generate between 3 to 6 steps for each recipe
+
+            for ($i = 1; $i <= $numberOfSteps; $i++) {
+                DB::table('steps')->insert([
+                    'recipe_id' => $recipeId,
+                    'step_number' => $i,
+                    'description' => 'Step ' . $i . ' description for recipe ' . $recipeId,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }
     }
 }
